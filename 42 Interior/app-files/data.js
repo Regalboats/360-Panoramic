634f5ff8e5070968e7a34e4a9b8e84eac85b7148
Yacht Design Works <tak@yachtdesignworks.com>
2020-12-30 12:11:31 -0500

    Concepts Update

--- 42 Interior/app-files/data.js
+++ 42 Interior/app-files/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-master-berth",
-      "name": "Master Berth",
+      "id": "0-island-time---master--refreshment",
+      "name": "Island Time - Master & Refreshment",
       "levels": [
         {
           "tileSize": 256,
@@ -24,35 +24,41 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 1.6870827609810286,
-        "pitch": 0.6864343066496481,
-        "fov": 1.3715802068843215
+        "yaw": 1.6215241201772468,
+        "pitch": 0.3477859306832194,
+        "fov": 1.4256478217424775
       },
       "linkHotspots": [
         {
-          "yaw": -2.973604934658308,
-          "pitch": 0.9781479920228477,
-          "rotation": 0.7853981633974483,
-          "target": "2-head-a"
+          "yaw": -1.3306417845900569,
+          "pitch": 0.7312110991847369,
+          "rotation": 0,
+          "target": "2-island-time---aft-berth"
         },
         {
-          "yaw": -0.8252483969167841,
-          "pitch": 0.9541317564806242,
+          "yaw": 2.8822809894821972,
+          "pitch": 0.6882730847853651,
           "rotation": 0,
-          "target": "1-refreshment"
+          "target": "1-island-time---head"
         },
         {
-          "yaw": -1.2171381868527185,
-          "pitch": 0.7028252248696454,
-          "rotation": 0,
-          "target": "3-aft-berth"
+          "yaw": -1.8497795703659996,
+          "pitch": 0.020352629913480413,
+          "rotation": 4.71238898038469,
+          "target": "3-golden-hour---master--refreshment"
+        },
+        {
+          "yaw": -1.8484851794423545,
+          "pitch": 0.20128933787024295,
+          "rotation": 4.71238898038469,
+          "target": "6-moon-night---master--refreshment"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-refreshment",
-      "name": "Refreshment",
+      "id": "1-island-time---head",
+      "name": "Island Time - Head",
       "levels": [
         {
           "tileSize": 256,
@@ -74,35 +80,23 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": -2.8301712324353225,
-        "pitch": 0.4439326898497562,
-        "fov": 1.3715802068843215
+        "yaw": 1.6254164235785105,
+        "pitch": 0.7145881653302872,
+        "fov": 1.4256478217424775
       },
       "linkHotspots": [
         {
-          "yaw": 2.1690498626258865,
-          "pitch": 0.7919594749754815,
-          "rotation": 5.497787143782138,
-          "target": "3-aft-berth"
-        },
-        {
-          "yaw": -1.0077961729381961,
-          "pitch": 1.0144811322266225,
+          "yaw": -1.7281086516968518,
+          "pitch": 0.9099588143477568,
           "rotation": 0,
-          "target": "0-master-berth"
-        },
-        {
-          "yaw": 0.1154389655791821,
-          "pitch": 0.9417296098735637,
-          "rotation": 0,
-          "target": "2-head-a"
+          "target": "0-island-time---master--refreshment"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-head-a",
-      "name": "Head A",
+      "id": "2-island-time---aft-berth",
+      "name": "Island Time - Aft berth",
       "levels": [
         {
           "tileSize": 256,
@@ -124,29 +118,23 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 2.089660708591124,
-        "pitch": 0.6761490049487726,
-        "fov": 1.3715802068843215
+        "yaw": 2.152900826564027,
+        "pitch": 0.38423077669745354,
+        "fov": 1.4256478217424775
       },
       "linkHotspots": [
         {
-          "yaw": -1.6615965326818305,
-          "pitch": 0.9653710511463576,
+          "yaw": -0.04090937465496225,
+          "pitch": 0.2667032861869245,
           "rotation": 0,
-          "target": "1-refreshment"
-        },
-        {
-          "yaw": -0.19488962472347637,
-          "pitch": 0.8551007510926496,
-          "rotation": 0,
-          "target": "0-master-berth"
+          "target": "0-island-time---master--refreshment"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3-aft-berth",
-      "name": "Aft Berth",
+      "id": "3-golden-hour---master--refreshment",
+      "name": "Golden Hour - Master & Refreshment",
       "levels": [
         {
           "tileSize": 256,
@@ -168,26 +156,252 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 2.0413806719698115,
-        "pitch": 0.33166457904340163,
-        "fov": 1.3715802068843215
+        "yaw": 1.737969910190226,
+        "pitch": 0.37116180048141345,
+        "fov": 1.4256478217424775
       },
       "linkHotspots": [
         {
-          "yaw": -0.12640139130213868,
-          "pitch": 0.37172144924649153,
+          "yaw": -1.7940862508059698,
+          "pitch": -0.16340138221063505,
+          "rotation": 4.71238898038469,
+          "target": "0-island-time---master--refreshment"
+        },
+        {
+          "yaw": -1.2796797717444637,
+          "pitch": 0.7177186543477028,
           "rotation": 0,
-          "target": "1-refreshment"
+          "target": "5-golden-hour---aft-berth"
+        },
+        {
+          "yaw": 2.94251412064649,
+          "pitch": 0.580248342788563,
+          "rotation": 0,
+          "target": "4-golden-hour---head"
+        },
+        {
+          "yaw": -1.79245497702372,
+          "pitch": 0.20338692086971122,
+          "rotation": 4.71238898038469,
+          "target": "6-moon-night---master--refreshment"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-golden-hour---head",
+      "name": "Golden Hour - Head",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.63272426632198,
+        "pitch": 0.7278701243547232,
+        "fov": 1.4256478217424775
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6514041382475959,
+          "pitch": 0.90953014717606,
+          "rotation": 0,
+          "target": "3-golden-hour---master--refreshment"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-golden-hour---aft-berth",
+      "name": "Golden Hour - Aft berth",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05288636743402897,
+          "pitch": 0.33010074418147894,
+          "rotation": 0,
+          "target": "3-golden-hour---master--refreshment"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-moon-night---master--refreshment",
+      "name": "Moon Night - Master & Refreshment",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6153162226880022,
+        "pitch": 0.40800442857323915,
+        "fov": 1.4256478217424775
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8190036793266806,
+          "pitch": 0.6130470091521865,
+          "rotation": 0,
+          "target": "7-moon-night---head"
+        },
+        {
+          "yaw": -1.9045047768521606,
+          "pitch": 0.018302894908302392,
+          "rotation": 4.71238898038469,
+          "target": "3-golden-hour---master--refreshment"
+        },
+        {
+          "yaw": -1.8989735440155453,
+          "pitch": -0.16616798738654737,
+          "rotation": 4.71238898038469,
+          "target": "0-island-time---master--refreshment"
+        },
+        {
+          "yaw": -1.3584426921035693,
+          "pitch": 0.6952851585017648,
+          "rotation": 0,
+          "target": "8-moon-night---aft-berth"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-moon-night---head",
+      "name": "Moon Night - Head",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.2810911258393052,
+        "pitch": 0.7913095384445299,
+        "fov": 1.4256478217424775
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9870072023536736,
+          "pitch": 0.7432163693294953,
+          "rotation": 0,
+          "target": "6-moon-night---master--refreshment"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-moon-night---aft-berth",
+      "name": "Moon Night - Aft berth",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.3180989537118126,
+        "pitch": 0.46077392169165776,
+        "fov": 1.4256478217424775
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.02737406386509811,
+          "pitch": 0.1715604913131017,
+          "rotation": 0,
+          "target": "6-moon-night---master--refreshment"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "42 Interior Concept",
+  "name": "42 Interior Concepts",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
-    "fullscreenButton": false,
+    "fullscreenButton": true,
     "viewControlButtons": false
   }
 };
