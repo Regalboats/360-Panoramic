806ca2fbe9c319621a1f85e438a46eea46bc7b1c
Yacht Design Works <31591598+YachtDesignWorks@users.noreply.github.com>
2022-08-11 09:53:30 -0400

    48 SAV

---
+++ 48-sav---preliminary/app-files/data.js
@@ -0,0 +1,553 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bow",
+      "name": "Bow",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 1.3154510334385474,
+        "pitch": 0.4275410347507069,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8031772153345855,
+          "pitch": 0.14609628089881355,
+          "rotation": 5.497787143782138,
+          "target": "1-mid-deck_port"
+        },
+        {
+          "yaw": 0.8890530939051651,
+          "pitch": 0.0705215943874542,
+          "rotation": 1.5707963267948966,
+          "target": "2-mid-deck_stb"
+        },
+        {
+          "yaw": 1.2967237909994616,
+          "pitch": 0.3753743984209148,
+          "rotation": 3.141592653589793,
+          "target": "7-interior---fwd-berth"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-mid-deck_port",
+      "name": "Mid Deck_Port",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 0.37023446719932984,
+        "pitch": 0.48088979100820595,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8125910121695625,
+          "pitch": 0.29588080825476126,
+          "rotation": 0,
+          "target": "3-wet-bar"
+        },
+        {
+          "yaw": 1.3091071675341865,
+          "pitch": 0.36173328495275214,
+          "rotation": 0,
+          "target": "4-aft-deck"
+        },
+        {
+          "yaw": 0.28767220565851304,
+          "pitch": 0.12946534299844537,
+          "rotation": 3.141592653589793,
+          "target": "2-mid-deck_stb"
+        },
+        {
+          "yaw": -1.7493116646858429,
+          "pitch": 0.2533391382811345,
+          "rotation": 0,
+          "target": "0-bow"
+        },
+        {
+          "yaw": -0.946873345455483,
+          "pitch": 0.6417739425484754,
+          "rotation": 6.283185307179586,
+          "target": "7-interior---fwd-berth"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-mid-deck_stb",
+      "name": "Mid Deck_STB",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": -2.7466197626496047,
+        "pitch": 0.6170084372978675,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.865756845662103,
+          "pitch": 0.76217573774899,
+          "rotation": 0,
+          "target": "3-wet-bar"
+        },
+        {
+          "yaw": -2.9899460087256458,
+          "pitch": 0.17856444786259473,
+          "rotation": 3.141592653589793,
+          "target": "1-mid-deck_port"
+        },
+        {
+          "yaw": 1.7961025714952426,
+          "pitch": 0.5032420215910367,
+          "rotation": 3.141592653589793,
+          "target": "4-aft-deck"
+        },
+        {
+          "yaw": 1.1175636274919807,
+          "pitch": 0.44422041995161266,
+          "rotation": 3.141592653589793,
+          "target": "6-wing_stb"
+        },
+        {
+          "yaw": -1.864975062861685,
+          "pitch": 0.010381343917265085,
+          "rotation": 10.210176124166829,
+          "target": "0-bow"
+        },
+        {
+          "yaw": -2.5159448474610038,
+          "pitch": 0.08609831173617799,
+          "rotation": 3.141592653589793,
+          "target": "7-interior---fwd-berth"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-wet-bar",
+      "name": "Wet Bar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 1.3252253176682203,
+        "pitch": 0.7532097961591049,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2988273793873333,
+          "pitch": 0.538264860862899,
+          "rotation": 9.42477796076938,
+          "target": "4-aft-deck"
+        },
+        {
+          "yaw": -0.2973533512661479,
+          "pitch": 0.7584495840901901,
+          "rotation": 3.141592653589793,
+          "target": "2-mid-deck_stb"
+        },
+        {
+          "yaw": -2.5748801197354254,
+          "pitch": 0.2542572461294519,
+          "rotation": 3.141592653589793,
+          "target": "1-mid-deck_port"
+        },
+        {
+          "yaw": -2.2725283178832356,
+          "pitch": 0.06196010649198058,
+          "rotation": 0.7853981633974483,
+          "target": "0-bow"
+        },
+        {
+          "yaw": 0.7505630035740829,
+          "pitch": 0.3719431326287026,
+          "rotation": 3.141592653589793,
+          "target": "6-wing_stb"
+        },
+        {
+          "yaw": 1.960921779837273,
+          "pitch": 0.38086020043363256,
+          "rotation": 3.141592653589793,
+          "target": "6-wing_stb"
+        },
+        {
+          "yaw": -2.117522300226657,
+          "pitch": 0.2711706744418656,
+          "rotation": 0,
+          "target": "7-interior---fwd-berth"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-aft-deck",
+      "name": "Aft Deck",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": -2.1497957208877523,
+        "pitch": 0.24404608992028365,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8127703839430733,
+          "pitch": 0.18241079052685194,
+          "rotation": 3.141592653589793,
+          "target": "3-wet-bar"
+        },
+        {
+          "yaw": -3.1197762956499595,
+          "pitch": 0.5425011123723706,
+          "rotation": 3.141592653589793,
+          "target": "5-wing_port"
+        },
+        {
+          "yaw": 0.0763908071339543,
+          "pitch": 0.5308591412549646,
+          "rotation": 3.141592653589793,
+          "target": "6-wing_stb"
+        },
+        {
+          "yaw": -1.2468075454823975,
+          "pitch": 0.11730341746743278,
+          "rotation": 5.497787143782138,
+          "target": "2-mid-deck_stb"
+        },
+        {
+          "yaw": -2.3959223311828612,
+          "pitch": 0.1006562785244185,
+          "rotation": 7.0685834705770345,
+          "target": "1-mid-deck_port"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-wing_port",
+      "name": "Wing_Port",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": -0.5345518977149002,
+        "pitch": 0.46650390426772326,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.18653525621118483,
+          "pitch": 0.49352279562813806,
+          "rotation": 9.42477796076938,
+          "target": "4-aft-deck"
+        },
+        {
+          "yaw": -0.07217015760095258,
+          "pitch": 0.1826740523696344,
+          "rotation": 3.141592653589793,
+          "target": "6-wing_stb"
+        },
+        {
+          "yaw": -1.183946375335987,
+          "pitch": 0.12580732791968252,
+          "rotation": 3.141592653589793,
+          "target": "3-wet-bar"
+        },
+        {
+          "yaw": -1.5956563473350727,
+          "pitch": 0.1822249072657467,
+          "rotation": 3.141592653589793,
+          "target": "1-mid-deck_port"
+        },
+        {
+          "yaw": -1.707206147978921,
+          "pitch": -0.03885345905174198,
+          "rotation": 2.356194490192345,
+          "target": "0-bow"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-wing_stb",
+      "name": "Wing_STB",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": -2.6097655779764395,
+        "pitch": 0.32567009019416204,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0384094457617064,
+          "pitch": 0.28361717775997697,
+          "rotation": 3.141592653589793,
+          "target": "5-wing_port"
+        },
+        {
+          "yaw": -2.9010969876019796,
+          "pitch": 0.49140522078686466,
+          "rotation": 9.42477796076938,
+          "target": "4-aft-deck"
+        },
+        {
+          "yaw": -2.3626565172681655,
+          "pitch": 0.13319927615827964,
+          "rotation": 3.141592653589793,
+          "target": "3-wet-bar"
+        },
+        {
+          "yaw": -1.9546460489089341,
+          "pitch": 0.30926142230204334,
+          "rotation": 3.141592653589793,
+          "target": "2-mid-deck_stb"
+        },
+        {
+          "yaw": -1.8633353289229007,
+          "pitch": -0.04574819493151061,
+          "rotation": 10.210176124166829,
+          "target": "0-bow"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-interior---fwd-berth",
+      "name": "Interior - Fwd Berth",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.5997168029135027,
+        "pitch": 0.5722986979979385,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.389738683122536,
+          "pitch": 0.45547316784585234,
+          "rotation": 0,
+          "target": "8-interior---head--shower"
+        },
+        {
+          "yaw": 2.985729915745159,
+          "pitch": 0.4923963153490565,
+          "rotation": 7.0685834705770345,
+          "target": "9-interior---mid-berth"
+        },
+        {
+          "yaw": -2.1633977355377123,
+          "pitch": 0.04834089191062674,
+          "rotation": 0,
+          "target": "1-mid-deck_port"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-interior---head--shower",
+      "name": "Interior - Head & Shower",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.655726384601734,
+        "pitch": 0.8815879430380491,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0829880461360197,
+          "pitch": 0.9390154544459222,
+          "rotation": 0,
+          "target": "7-interior---fwd-berth"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-interior---mid-berth",
+      "name": "Interior - Mid Berth",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.303241031363621,
+        "pitch": 0.41476656417756885,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.028194903164973,
+          "pitch": -0.046258094731701505,
+          "rotation": 0,
+          "target": "7-interior---fwd-berth"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "48 SAV - Preliminary",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
