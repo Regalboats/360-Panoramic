46ddf55fcac5904faebcff509f33b08a11b24a38
Yacht Design Works <tak@yachtdesignworks.com>
2023-09-20 17:00:20 -0400

    FireFly B

---
+++ 20230920-firefly_v-berth_b/app-files/data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-seated",
+      "name": "Seated",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.065950820438177,
+        "pitch": 0.6154655808624607,
+        "fov": 1.352817585576841
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.98984659568882,
+          "pitch": 0.7660552091101316,
+          "rotation": 3.141592653589793,
+          "target": "1-stand"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-stand",
+      "name": "Stand",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.9036616116333027,
+        "pitch": 0.8378918967473936,
+        "fov": 1.352817585576841
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5235502630079534,
+          "pitch": 0.9346384274656785,
+          "rotation": 3.141592653589793,
+          "target": "1-stand"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "20230920 FireFly_V-berth_B",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
