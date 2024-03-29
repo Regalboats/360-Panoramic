0af2cd1c841bb0ac6f0568fee4168d1965b2f601
Yacht Design Works <tak@yachtdesignworks.com>
2024-03-29 12:36:59 -0400

    FireFly Foxtrot

---
+++ 20240329 FireFly GA_Foxtrot/app-files/data.js
@@ -0,0 +1,291 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-aft-stb-side",
+      "name": "Aft STB side",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.321639719498787,
+        "pitch": 0.23844968148705803,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7058027847473696,
+          "pitch": 0.280160308938747,
+          "rotation": 3.141592653589793,
+          "target": "1-wet-bar"
+        },
+        {
+          "yaw": 1.9163579541530185,
+          "pitch": 0.1008375794041001,
+          "rotation": 3.141592653589793,
+          "target": "3-helm"
+        },
+        {
+          "yaw": 1.316141938944643,
+          "pitch": 0.2525255715210939,
+          "rotation": 3.141592653589793,
+          "target": "2-cockpit"
+        },
+        {
+          "yaw": 1.5633137348422572,
+          "pitch": 0.05641631057796559,
+          "rotation": 3.141592653589793,
+          "target": "4-walkthrough"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-wet-bar",
+      "name": "Wet bar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.3934464308326842,
+        "pitch": 0.4555961976348364,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0578583773140666,
+          "pitch": 0.32834226817666057,
+          "rotation": 3.141592653589793,
+          "target": "0-aft-stb-side"
+        },
+        {
+          "yaw": 1.0285481404817887,
+          "pitch": 0.21673348356676314,
+          "rotation": 3.141592653589793,
+          "target": "2-cockpit"
+        },
+        {
+          "yaw": 1.264420050007912,
+          "pitch": 0.08112029028652046,
+          "rotation": 3.141592653589793,
+          "target": "3-helm"
+        },
+        {
+          "yaw": 0.7429922071087134,
+          "pitch": 0.06635023163598675,
+          "rotation": 3.141592653589793,
+          "target": "4-walkthrough"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-cockpit",
+      "name": "Cockpit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.7129306856464535,
+        "pitch": 0.36141951919606186,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4782561968162042,
+          "pitch": 0.2800780709309283,
+          "rotation": 3.141592653589793,
+          "target": "3-helm"
+        },
+        {
+          "yaw": 0.368850613008302,
+          "pitch": 0.20887637632791822,
+          "rotation": 3.141592653589793,
+          "target": "4-walkthrough"
+        },
+        {
+          "yaw": -1.9047325140394165,
+          "pitch": 0.2987414019356258,
+          "rotation": 3.141592653589793,
+          "target": "1-wet-bar"
+        },
+        {
+          "yaw": -2.9969496226418215,
+          "pitch": 0.19509132249922345,
+          "rotation": 3.141592653589793,
+          "target": "0-aft-stb-side"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-helm",
+      "name": "Helm",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.391493023714915,
+        "pitch": 0.49457103236886724,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6086280140409581,
+          "pitch": 0.3280075106022995,
+          "rotation": 3.141592653589793,
+          "target": "2-cockpit"
+        },
+        {
+          "yaw": 0.23951048578618384,
+          "pitch": 0.2550895297235467,
+          "rotation": 3.141592653589793,
+          "target": "4-walkthrough"
+        },
+        {
+          "yaw": -0.9639081198562316,
+          "pitch": 0.1012399773890813,
+          "rotation": 3.141592653589793,
+          "target": "1-wet-bar"
+        },
+        {
+          "yaw": -1.6727455910685194,
+          "pitch": 0.11671734785488042,
+          "rotation": 3.141592653589793,
+          "target": "0-aft-stb-side"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-walkthrough",
+      "name": "Walkthrough",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.7103343093458783,
+        "pitch": 0.3302341543038665,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.256568267915016,
+          "pitch": 0.298869054083859,
+          "rotation": 3.141592653589793,
+          "target": "3-helm"
+        },
+        {
+          "yaw": 2.1285732953337186,
+          "pitch": 0.24873319827945295,
+          "rotation": 3.141592653589793,
+          "target": "2-cockpit"
+        },
+        {
+          "yaw": 2.384959423774874,
+          "pitch": 0.11354215356040243,
+          "rotation": 9.42477796076938,
+          "target": "1-wet-bar"
+        },
+        {
+          "yaw": 1.9011229104404475,
+          "pitch": 0.06773751965144648,
+          "rotation": 3.141592653589793,
+          "target": "0-aft-stb-side"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "20240329 FireFly GA_Foxtrot ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
