005c0882ec7990cff085c0b217217773cb2fb23d
Regal Boats <68389311+Regalboats@users.noreply.github.com>
2024-08-13 16:39:10 -0400

    SDlll Walkthrough

---
+++ 20240813-sdll-walkthrough/app-files/data.js
@@ -0,0 +1,597 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-helm-",
+      "name": "Helm ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6005866079288218,
+        "pitch": 0.1820525356293352,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2853789303460115,
+          "pitch": 0.2866092926994952,
+          "rotation": 3.141592653589793,
+          "target": "5-wetbar"
+        },
+        {
+          "yaw": -0.43420501689296387,
+          "pitch": 0.23333962701195432,
+          "rotation": 3.141592653589793,
+          "target": "6-mid-port"
+        },
+        {
+          "yaw": 1.563297872854382,
+          "pitch": 0.06107185894565248,
+          "rotation": 3.141592653589793,
+          "target": "1-bow"
+        },
+        {
+          "yaw": 0.6434487510884566,
+          "pitch": 0.44042624533578234,
+          "rotation": 0.7853981633974483,
+          "target": "10-entrace"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bow",
+      "name": "Bow",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.802056593026494,
+        "pitch": 0.4829712283979575,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5453231156806693,
+          "pitch": 0.041592085682758295,
+          "rotation": 3.141592653589793,
+          "target": "0-helm-"
+        },
+        {
+          "yaw": 1.936375041483684,
+          "pitch": 0.07450533577720897,
+          "rotation": 3.9269908169872414,
+          "target": "6-mid-port"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-stbd-fold-down",
+      "name": "STBD Fold Down",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.2593235968522087,
+        "pitch": 0.41196730877044274,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2695844174639923,
+          "pitch": 0.49247701526416243,
+          "rotation": 3.141592653589793,
+          "target": "4-aft-cockpit"
+        },
+        {
+          "yaw": 0.9374924099852286,
+          "pitch": 0.2861367151762515,
+          "rotation": 3.141592653589793,
+          "target": "3-port-side-fold-down"
+        },
+        {
+          "yaw": 1.7450762552811865,
+          "pitch": 0.09636942746206145,
+          "rotation": 3.141592653589793,
+          "target": "5-wetbar"
+        },
+        {
+          "yaw": 2.11586142000632,
+          "pitch": 0.06454192300741468,
+          "rotation": 3.9269908169872414,
+          "target": "0-helm-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-port-side-fold-down",
+      "name": "Port Side Fold Down",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.8362787570667836,
+        "pitch": 0.35997627464895743,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.795510478572643,
+          "pitch": 0.2495529594033723,
+          "rotation": 3.141592653589793,
+          "target": "2-stbd-fold-down"
+        },
+        {
+          "yaw": 0.8817966968481237,
+          "pitch": 0.09082898462736644,
+          "rotation": 3.141592653589793,
+          "target": "5-wetbar"
+        },
+        {
+          "yaw": 0.32218425865729117,
+          "pitch": 0.16202464851327747,
+          "rotation": 3.141592653589793,
+          "target": "6-mid-port"
+        },
+        {
+          "yaw": 1.579632490271047,
+          "pitch": 0.4810274709474083,
+          "rotation": 3.141592653589793,
+          "target": "4-aft-cockpit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-aft-cockpit",
+      "name": "Aft Cockpit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.1598640661030366,
+        "pitch": 0.4917946259033492,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08149281155707655,
+          "pitch": 0.3724196523523311,
+          "rotation": 0,
+          "target": "3-port-side-fold-down"
+        },
+        {
+          "yaw": -2.9318872710729096,
+          "pitch": 0.34837112683726446,
+          "rotation": 0,
+          "target": "2-stbd-fold-down"
+        },
+        {
+          "yaw": 1.5658089601933156,
+          "pitch": 0.10915789422343636,
+          "rotation": 3.141592653589793,
+          "target": "5-wetbar"
+        },
+        {
+          "yaw": 0.8953022742876566,
+          "pitch": 0.17044759924338138,
+          "rotation": 2.356194490192345,
+          "target": "6-mid-port"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-wetbar",
+      "name": "Wetbar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.133959673259712,
+        "pitch": 0.6293723958129362,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04022578265789711,
+          "pitch": 0.4893315969397669,
+          "rotation": 3.141592653589793,
+          "target": "6-mid-port"
+        },
+        {
+          "yaw": -0.9503495566053068,
+          "pitch": 0.29302150869882126,
+          "rotation": 0.7853981633974483,
+          "target": "3-port-side-fold-down"
+        },
+        {
+          "yaw": -1.5866038447677902,
+          "pitch": 0.39843572889956036,
+          "rotation": 3.141592653589793,
+          "target": "4-aft-cockpit"
+        },
+        {
+          "yaw": -2.099183378179738,
+          "pitch": 0.2716338858827996,
+          "rotation": 5.497787143782138,
+          "target": "2-stbd-fold-down"
+        },
+        {
+          "yaw": 1.8474686697908105,
+          "pitch": 0.3617009322524538,
+          "rotation": 3.141592653589793,
+          "target": "0-helm-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-mid-port",
+      "name": "Mid port",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.5047395259869063,
+        "pitch": 0.45962668006504614,
+        "fov": 1.4622714028694404
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7919699226262704,
+          "pitch": 0.44168463724221496,
+          "rotation": 3.141592653589793,
+          "target": "5-wetbar"
+        },
+        {
+          "yaw": -1.3281121360176122,
+          "pitch": 0.34204604177248044,
+          "rotation": 1.5707963267948966,
+          "target": "3-port-side-fold-down"
+        },
+        {
+          "yaw": -2.0458087405995666,
+          "pitch": 0.33216410656547346,
+          "rotation": 3.141592653589793,
+          "target": "4-aft-cockpit"
+        },
+        {
+          "yaw": -2.3324061775553737,
+          "pitch": 0.23348969353945925,
+          "rotation": 5.497787143782138,
+          "target": "2-stbd-fold-down"
+        },
+        {
+          "yaw": 1.7002574562654509,
+          "pitch": 0.10022668955061498,
+          "rotation": 1.5707963267948966,
+          "target": "1-bow"
+        },
+        {
+          "yaw": 2.723720737711865,
+          "pitch": 0.25512251456701485,
+          "rotation": 3.141592653589793,
+          "target": "0-helm-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-head",
+      "name": "Head",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.1243577771455318,
+        "pitch": 0.546492494022214,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8118172915670137,
+          "pitch": 0.24715199936471777,
+          "rotation": 0,
+          "target": "10-entrace"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-midberth",
+      "name": "Midberth",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.185788200053464,
+        "pitch": 0.10809670505852509,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.737707040211295,
+          "pitch": -0.08851577483980755,
+          "rotation": 3.141592653589793,
+          "target": "10-entrace"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-interior-dinette",
+      "name": "Interior Dinette",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.027737608659649,
+        "pitch": 0.47332226635009356,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2795648462832432,
+          "pitch": 0.49368113801664215,
+          "rotation": 0,
+          "target": "8-midberth"
+        },
+        {
+          "yaw": 0.9409712463600819,
+          "pitch": 0.18170766320121778,
+          "rotation": 4.71238898038469,
+          "target": "7-head"
+        },
+        {
+          "yaw": 1.6401605659231162,
+          "pitch": 0.3370534465390307,
+          "rotation": 9.42477796076938,
+          "target": "10-entrace"
+        },
+        {
+          "yaw": 1.6133635603736085,
+          "pitch": -0.07311319566706587,
+          "rotation": 5.497787143782138,
+          "target": "0-helm-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-entrace",
+      "name": "Entrace",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.0794754515853757,
+        "pitch": 0.45898022007951056,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7680738118367234,
+          "pitch": 0.4160795087094087,
+          "rotation": 3.141592653589793,
+          "target": "9-interior-dinette"
+        },
+        {
+          "yaw": -2.517388753664015,
+          "pitch": 0.7252620067039324,
+          "rotation": 0.7853981633974483,
+          "target": "8-midberth"
+        },
+        {
+          "yaw": 2.749656780275936,
+          "pitch": 0.3781244477190526,
+          "rotation": 0,
+          "target": "7-head"
+        },
+        {
+          "yaw": -1.9792258753045697,
+          "pitch": -0.006552030536902009,
+          "rotation": 0,
+          "target": "0-helm-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "20240813 SDll Walkthrough",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
