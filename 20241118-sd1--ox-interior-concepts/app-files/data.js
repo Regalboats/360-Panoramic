9b8a50b1419eb07d531aeb2cbd955cc7e2122e3c
Tak <65673253+Yacht-Design-Works@users.noreply.github.com>
2024-11-18 10:11:31 -0500

    SDI & OX Interior

---
+++ 20241118-sd1--ox-interior-concepts/app-files/data.js
@@ -0,0 +1,139 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sd-i-interior",
+      "name": "SD I Interior",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 2.8216411909970747,
+        "pitch": 0.8872953639145358,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-ox-interior---v-berth",
+      "name": "OX Interior - V berth",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.9402580082873238,
+        "pitch": 0.5549705164797913,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-ox-interior---l-shape",
+      "name": "OX Interior - L shape",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6497051521334392,
+        "pitch": 0.5717868768698011,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-ox-interior---full-bunk",
+      "name": "OX Interior - Full Bunk",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.1817413499729454,
+        "pitch": 0.5515319699955441,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "20241118 SDI & OX Interior concepts",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
